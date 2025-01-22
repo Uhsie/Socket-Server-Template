e1e59b08e09e1d2b04f885b42eddad202b6ebcf1
Uhsie <43722502+Uhsie@users.noreply.github.com>
2025-01-22 09:24:24 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,13 +3,10 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
-// require("dotenv").config();
 
 const serverPort = process.env.PORT || 3000;
 const server = http.createServer(app);
 const WebSocket = require("ws");
-
-let keepAliveId;
 
 const wss =
   process.env.NODE_ENV === "production"
@@ -23,31 +20,30 @@ wss.on("connection", function (ws, req) {
   console.log("Connection Opened");
   console.log("Client size: ", wss.clients.size);
 
-  if (wss.clients.size === 1) {
-    console.log("first connection. starting keepalive");
-    keepServerAlive();
-  }
-
+  // Handle message from client or admin
   ws.on("message", (data) => {
     let stringifiedData = data.toString();
+    
+    // If the message is "pong", we ignore it (client response to ping)
     if (stringifiedData === 'pong') {
       console.log('keepAlive');
       return;
     }
+
+    // Broadcast the message to all other clients
     broadcast(ws, stringifiedData, false);
   });
 
-  ws.on("close", (data) => {
-    console.log("closing connection");
+  ws.on("close", () => {
+    console.log("Closing connection");
 
     if (wss.clients.size === 0) {
-      console.log("last client disconnected, stopping keepAlive interval");
-      clearInterval(keepAliveId);
+      console.log("Last client disconnected");
     }
   });
 });
 
-// Implement broadcast function because of ws doesn't have it
+// Implement broadcast function because WebSocket doesn't have it
 const broadcast = (ws, message, includeSelf) => {
   if (includeSelf) {
     wss.clients.forEach((client) => {
@@ -64,20 +60,6 @@ const broadcast = (ws, message, includeSelf) => {
   }
 };
 
-/**
- * Sends a ping message to all connected clients every 50 seconds
- */
-//  const keepServerAlive = () => {
-//   keepAliveId = setInterval(() => {
-//     wss.clients.forEach((client) => {
-//       if (client.readyState === WebSocket.OPEN) {
-//         client.send('ping');
-//       }
-//     });
-//   }, 50000);
-// };
-
-
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
